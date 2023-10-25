cbc518dfbac7710c12373f81c145f4d9e91ca5e3
Faudev <fauezo@gmail.com>
2023-10-25 17:36:29 -0300

    CalculoRetangulo
    Exercício orientação de objeto C Charp

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+namespace CalcularRetangulo
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Entre a lagura e altura do retangulo");
+            Console.Write("Largura: ");
+             retangulo.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Altura: ");
+             retangulo.Altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+           
+            
+            Console.WriteLine("Area: " + retangulo.Area().ToString("f2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro: "+ retangulo.Perimeitro().ToString("f2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagnal: " + retangulo.Diagnal().ToString("f2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Retangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+namespace CalcularRetangulo
+{
+    internal class Retangulo
+    {
+        public Double Largura;
+        public Double Altura;
+
+        public Double Area()
+        {
+
+            double a = Largura * Altura;
+            return a ;
+        }
+
+        public double Perimeitro()
+        {
+            return 2*(Largura+Altura);
+
+        }
+
+        public double Diagnal() 
+        {
+            double d = Math.Sqrt(Math.Pow(Largura, 2)+ Math.Pow(Altura,2)); 
+            return d;
+        
+        }
+    }
+}
